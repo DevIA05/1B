1075358ecf25a36251c1356e0c10a78b773cf1b4
Saia Ralandison <ralandison.saia@gmail.com>
2022-11-04 10:14:17 +0400

    page d'authentification créé
    page authentification crée avec des liens prêt à être utilisé

--- src/identification/urls.py
+++ src/identification/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login', views.login_user, name="login"),
     path('', views.logout_user, name="logout"),
+    path('PA', views.page_aut, name="page_aut")
 ]
--- src/identification/views.py
+++ src/identification/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.template import loader
 
 # Create your views here.
 def login_user(request):
@@ -23,3 +24,7 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You Were Logged Out!"))
     return redirect('login')
+
+def page_aut(request):
+    template = loader.get_template ('page_aut.html')
+    return HttpResponse(template.render())
