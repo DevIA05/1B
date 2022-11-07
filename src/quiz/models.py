2ebef576d5297ea33dac25666aba53df5588937e
DevIA.E03 <DevIA.E03@afpar.com>
2022-11-07 09:21:47 +0400

    Update models.py

--- src/quiz/models.py
+++ src/quiz/models.py
@@ -46,12 +46,13 @@ class MyUserManager(BaseUserManager):
         user.save()
         return user
 
+
 class Secteur(models.Model):
-    nomSecteur = models.CharField(verbose_name = "nomSecteur", max_length=50)
-    codeSecteur = models.CharField(verbose_name = "codeSecteur", max_length=3, primary_key=True)
-    
+    codesecteur = models.CharField(db_column='codeSecteur', primary_key=True, max_length=10)  # Field name made lowercase.
+    nomsecteur = models.CharField(db_column='nomSecteur', max_length=20)  # Field name made lowercase.
+
     class Meta:
-        db_table = "Secteur"
+        db_table = 'secteur'
     
 class Personnel(AbstractBaseUser): #models.Model #AbstractBaseUser
     prenom = models.CharField(verbose_name = "prenom", max_length=20)
@@ -75,14 +76,55 @@ class Personnel(AbstractBaseUser): #models.Model #AbstractBaseUser
         db_table = "Personnel"
 
 
+
 class Collaborateur(models.Model):
     matricule = models.OneToOneField('Personnel', models.DO_NOTHING, db_column='matricule', primary_key=True)
-    
-    class Meta: db_table = 'Collaborateur'
-    
+
+    class Meta:
+        db_table = 'collaborateur'
+           
 
 class Superuser(models.Model):
     matricule = models.OneToOneField(Personnel, models.DO_NOTHING, db_column='matricule', primary_key=True)
     role = models.BooleanField()
 
-    class Meta: db_table = 'Superuser'
+    class Meta:
+        db_table = 'superuser'
+        
+
+
+class Historique(models.Model):
+    idhisto = models.AutoField(db_column='idHisto', primary_key=True)  # Field name made lowercase.
+    matricule = models.ForeignKey(Collaborateur, models.DO_NOTHING, db_column='matricule')
+    idsession = models.ForeignKey('Sessionquizz', models.DO_NOTHING, db_column='idSession')  # Field name made lowercase.
+    score = models.IntegerField(blank=True, null=True)
+    dateparticipation = models.DateField(db_column='dateParticipation', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        db_table = 'historique'
+
+
+
+class Quizz(models.Model):
+    idquizz = models.AutoField(db_column='idQuizz', primary_key=True)  # Field name made lowercase.
+    nomfichier = models.CharField(db_column='nomFichier', max_length=30)  # Field name made lowercase.
+    urlfichier = models.CharField(db_column='urlFichier', max_length=200)  # Field name made lowercase.
+    codesecteur = models.ForeignKey('Secteur', models.DO_NOTHING, db_column='codeSecteur')  # Field name made lowercase.
+    matricule = models.ForeignKey('Superuser', models.DO_NOTHING, db_column='matricule', blank=True, null=True)
+
+    class Meta:
+        db_table = 'quizz'
+
+
+
+class Sessionquizz(models.Model):
+    idsession = models.AutoField(db_column='idSession', primary_key=True)  # Field name made lowercase.
+    evaluation = models.BooleanField()
+    datecreation = models.DateField(db_column='dateCreation')  # Field name made lowercase.
+    dateexpiration = models.DateField(db_column='dateExpiration', blank=True, null=True)  # Field name made lowercase.
+    matricule = models.ForeignKey('Superuser', models.DO_NOTHING, db_column='matricule')
+    idquizz = models.ForeignKey(Quizz, models.DO_NOTHING, db_column='idQuizz')  # Field name made lowercase.
+    timer = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'sessionQuizz'
