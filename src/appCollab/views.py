d5f2a1c3a3371463eb297a5ea8cb2ea3638a2d71
DevIA05 <dia05@polenumerique.re>
2022-11-15 16:07:29 +0400

    mise en place: sauvegarde d'une variable avec à la méthode session

--- src/appCollab/views.py
+++ src/appCollab/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render
 from quiz.models import Quizz, Personnel, Collaborateur, Superuser
 import json, os
 
-data = {}
-
 def pa1(request):
     return render(request, 'page_accueil.html')
 
@@ -13,22 +11,27 @@ def ce(request):
     return render(request, 'condi_exam.html')
 
 
-def loadQuiz():
+# def loadQuiz(request):
     # data = {'@goodResp': '3', '@time': '00:00:30.00', '@coeff': '1', 
     #         'title': "title", 
     #         'question': 'q1 :', 
     #         'listeResp': {'response': ['resp1', 'resp2', 'resp3']}}
-    with open("qjson/31.quv.json") as f: data = json.load(f)
-    quizHTML(data)
+    # with open("qjson/31.quv.json") as f: data = json.load(f)
+    # request.session['task_created'] = "ok"
+    # quizHTML(data, 0)
 
-def quizHTML(data, numQuestion):
+def quizHTML(request):
+    with open("qjson/31.quv.json") as f: data = json.load(f)
+    request.session['pointsDuCandidat'] = 0
+    request.session['quiz'] = data
+    numQuestion = 0
     duree       = convertTimeToMin(data["questionnaire"]["question"][numQuestion]["@duree"])
     titre       = data["questionnaire"]["question"][numQuestion]["titre"]
     intitule    = data["questionnaire"]["question"][numQuestion]["intitule"]
     reponses    = data["questionnaire"]["question"][numQuestion]['listerep']["reponse"]
     nbReponses  = list(range(1, len(reponses)+1))
-    coeff       = data["questionnaire"]["question"][numQuestion]["@coeff"]
-    bonneRep    = data["questionnaire"]["question"][numQuestion]["@bonne"]
+    # coeff       = data["questionnaire"]["question"][numQuestion]["@coeff"]
+    # bonneRep    = data["questionnaire"]["question"][numQuestion]["@bonne"]
 
     return render(request, 'doTheQuiz.html', context={"duree"       : duree,
                                                       "titre"       : titre,
@@ -37,14 +40,6 @@ def quizHTML(data, numQuestion):
                                                       "numQuestion" : numQuestion})
 
 def nextQuestion(request):
-    numQuestion           = 0
-    pointsDuCandidat      = 0
-    pointDeLaQuestion     = 1
-
-    print("**********************")
-    print(request)
-    print("**********************")
-
     if request.method == "POST":
         checkedBox = request.POST.getlist('result[]')
         print(checkedBox)
