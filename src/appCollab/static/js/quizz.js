9e0a0badca2422c344650b6fa53810882d207199
DevIA05 <dia05@polenumerique.re>
2022-11-17 16:05:51 +0400

    mise à jour: setInterval est stopé à chaque validation (le compte à rebours s'accélérer

--- src/appCollab/static/js/quizz.js
+++ src/appCollab/static/js/quizz.js
@@ -6,7 +6,6 @@ function isChecked(){
     const inputElements = document.getElementsByClassName('reponse');
     for(var i=0; inputElements[i]; ++i){
         if(inputElements[i].checked){
-            // checkedBox.push(inputElements[i].name);
             checkedBox.push(i);
         }
     }
@@ -20,26 +19,28 @@ const form = document.getElementById('myForm')
 form.addEventListener('submit', sendData);
 function sendData(event){
     event.preventDefault();
-    console.log("sendData")
     const csrf  = $('input[name="csrfmiddlewaretoken"]').val()   // collect token
+    const respUser = isChecked()
     // ------------------- Send data to view -------------------
     $.ajax({
         type: "POST",
         url: 'nextQuestion', // Name of the django view that will retrieve the data
         data: {
             csrfmiddlewaretoken : csrf,
-            "result": isChecked()
+            "result": respUser,       // data to send
         },
         dataType: "json",
         // ------------------- Receiving data from the view -------------------
-        success: function (data) {
+        success: function (data) { // if send successful 
+            // Update variables with the new data received
             timerQuestion = data["data"]["duree"]
             titre         = data["data"]["titre"]
             intitule      = data["data"]["intitule"]
             listRep       = data["data"]["reponses"]
-            updateTag()
-            initTimer()
-            startTimer()
+            updateTag()  // Updates fields with the update variables
+            onTimesUp()  // Stop the setInterval method
+            initTimer()  // Resets the variables concerning the countdown 
+            startTimer() // Start the countdown
         },
         failure: function () {
             alert("failure");
@@ -51,8 +52,9 @@ function sendData(event){
 */
 const containerRep  = document.getElementById("container-reponses");
 function updateTag(){
-    document.getElementById("titre").textContent    = titre
-    document.getElementById("intitule").textContent = intitule
+    document.getElementById("titre").textContent    = titre    // title
+    document.getElementById("intitule").textContent = intitule // entitled
+    // Responses
     containerRep.innerHTML = "";
     const tagsRep = document.createElement("div");
     for (let r = 0; r < listRep.length; r++) {
