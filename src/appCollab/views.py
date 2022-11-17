3c06d3f468583a28157c0b0829b13cf6d34c49f7
DevIA05 <dia05@polenumerique.re>
2022-11-17 16:06:57 +0400

    mise à jour: récupération des données issus du template

--- src/appCollab/views.py
+++ src/appCollab/views.py
@@ -12,7 +12,12 @@ def pa1(request):
 def ce(request):
     return render(request, 'condi_exam.html')
 
-
+# ** 
+# Save data in session and send the first question
+# - quiz(dict)           : the question sheet
+# - numQuestion(int)     : question number
+# - pointsDuCandidat(int): score of the candidate over the questions
+# **
 def initQuiz(request):
     with open("qjson/31.quv.json") as f: data = json.load(f)
     request.session['pointsDuCandidat'] = 0
@@ -24,16 +29,25 @@ def initQuiz(request):
     # coeff       = data["questionnaire"]["question"][numQuestion]["@coeff"]
     # bonneRep    = data["questionnaire"]["question"][numQuestion]["@bonne"]
 
+# **
+# Submit next question
+# - incremente numQuestion
+# **
 def nextQuestion(request):
     if request.method == "POST":
         data = request.session.get("quiz")
         request.session["numQuestion"] = request.session.get('numQuestion') + 1
+        verifResponses(request.POST.getlist('result[]', False))
+        
         context = dataToDict(data, request.session.get('numQuestion'))
         return JsonResponse({"data":context})
 
-
+# ** 
+# Extracts data from quiz
+# return(dict) with only the elements for the realization of the quiz
+# **
 def dataToDict(data, numQuestion):
-    duree       = convertTimeToMin(data["questionnaire"]["question"][numQuestion]["@duree"])
+    duree       = convertTimeToSec(data["questionnaire"]["question"][numQuestion]["@duree"])
     titre       = data["questionnaire"]["question"][numQuestion]["titre"]
     intitule    = data["questionnaire"]["question"][numQuestion]["intitule"]
     reponses    = data["questionnaire"]["question"][numQuestion]['listerep']["reponse"]
@@ -43,7 +57,14 @@ def dataToDict(data, numQuestion):
              "reponses"    : reponses}
     return context
 
-def convertTimeToMin(t): 
-    s = t.split(':'); 
-    min = float(s[0])*3600+float(s[1])*60+float(s[2]);  
-    return min
+#**
+# Convert string time to float representing the second
+# return(float) second
+# ** 
+def convertTimeToSec(t): 
+    s = t.split(':');  # exemple: "00:00:30.00" -> ["00","00","30.00"]
+    sec = float(s[0])*3600+float(s[1])*60+float(s[2]);  
+    return sec
+
+def verifResponses(l):
+    pass
