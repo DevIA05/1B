5ded421f02d04ab955938e34ba420bd6f8eeced7
UgoDIA <112081131+UgoDIA@users.noreply.github.com>
2022-11-19 11:32:11 +0100

    merge idriss

--- src/quiz/urls.py
+++ src/quiz/urls.py
@@ -46,8 +46,8 @@ urlpatterns = [
     path('p2', login_required()(notAccessForCollaborateur(views.page2)), name="p2"),
     path('p3', views.page3, name="p3"),
     path('quiz/q', views.quiz, name="quiz"),
-    path('quizz/superuser/', include("appSuperUser.urls")),
-    path('quizz/collaborateur/', include('appCollab.urls')),
+    path('quiz/superuser/',include("appSuperUser.urls")),
+    path('quiz/collaborateur/', include('appCollab.urls')),
     path('test', views.test, name="test"),
 ]
 
