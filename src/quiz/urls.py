e662c905caf58e186742d99e91f25ac3ea5355e2
DevIA05 <dia05@polenumerique.re>
2022-11-20 12:44:03 +0400

    mise à jour: les restrictions d'accès aux pages s'appliquent sur l'importation des urls d'une app avec include

--- src/quiz/urls.py
+++ src/quiz/urls.py
@@ -17,16 +17,20 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from quiz.views import index
+from django.contrib import messages
+from django.shortcuts import redirect
 
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from decorator_include import decorator_include
 
 # Custom Decoration: Prevent access to the page
 def notAccessForCollaborateur(function):
     def wrapper(request, *args, **kw):
         user=request.user           # we get the user instance
         if(not user.isSuperUser()): # If user is not a superuser we redirect it to the unauthorized page of django
-            return HttpResponseRedirect('/unauthorized/')
+                messages.success(request, ("Vous n'êtes pas autorisé"))
+                return redirect('login')
         else:
             return function(request, *args, **kw)
     return wrapper
@@ -46,8 +50,8 @@ urlpatterns = [
     path('p2', login_required()(notAccessForCollaborateur(views.page2)), name="p2"),
     path('p3', views.page3, name="p3"),
     path('quiz/q', views.quiz, name="quiz"),
-    path('quiz/superuser/',include("appSuperUser.urls")),
-    path('quiz/collaborateur/', include('appCollab.urls')),
+    path('quiz/superuser/', decorator_include([login_required(login_url="login"), notAccessForCollaborateur], "appSuperUser.urls")), #include("appSuperUser.urls")),
+    path('quiz/collaborateur/', decorator_include([login_required(login_url="login")], "appCollab.urls")),
     path('test', views.test, name="test"),
 ]
 
